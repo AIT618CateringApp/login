1936dd8acd3acb225a598918c3be70b26a8c5e23
tjonsie3 <55417902+tjonsie3@users.noreply.github.com>
2019-11-26 09:22:26 -0500

    Add files via upload

--- project/app.js
+++ project/app.js
@@ -1,30 +1,34 @@
 const express = require("express");
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
-const bodyParser = require('body-parser');
-const path = require('path');
-require('dotenv').config();
+const MongoClient = require('mongodb').MongoClient;
+const db = "mongodb+srv://ait618Group:theProject@ait618-vldt3.mongodb.net/test?retryWrites=true&w=majority";
 
 
-const mongoose = require('mongoose');
+//connecting to mongo
+MongoClient.connect(db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true 
+}, function(err, db) {
 
-console.log(process.env.MONGO);
-mongoose.connect(process.env.MONGO,  { useNewUrlParser: true })
+	if (err) {
+		console.log(err); }
+		
+	else
+		console.log("mongodb connected");
+		db.close();
+});		
 
 //ejs
 app.use(expressLayouts);
-app.use(bodyParser.urlencoded({ extended: false }))
 app.set('view engine', 'ejs');
-app.use('/public', express.static(path.join(__dirname, 'public')))
 
-//DB config
-//const db = require('....')
-// connect to mongo
+// BodyParser -- getting data from the form with data.body
+app.use(express.urlencoded({ extended: false }));
 
 //routes 
 app.use('/', require('./routes/index'));
 app.use('/users', require('./routes/users'));
-app.use('/customer', require('./routes/customer'));
 
 
 app.listen(3000, console.log('Navagate to localhost: 3000'));
